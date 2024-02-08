baa74eff70376e32b022d5439dac48cc615b516b
Rinald Shabani <rinaldshabani@gmail.com>
2024-02-08 14:54:30 +0100

    Added pan and zoom controls to output diagram

--- components/terravision/Diagram.tsx
+++ components/terravision/Diagram.tsx
@@ -4,6 +4,8 @@
 import DiagramSkeleton from '@/components/ui/skeleton/Diagram';
 import { useTransitions } from '@/lib/useTransitions';
 
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
+
 const Diagram = () => {
   const {
     generationTransition: [generationPending]
@@ -14,17 +16,24 @@ const Diagram = () => {
       {generationPending ? (
         <DiagramSkeleton />
       ) : (
-        <img
-          alt="Output"
-          className="mx-auto my-4 p-8"
-          height="800"
-          src={`http://localhost:8001/terravision/output?timestamp=${Date.now()}`}
-          style={{
-            height: '100%',
-            objectFit: 'contain'
-          }}
-          width="800"
-        />
+        <TransformWrapper>
+          <TransformComponent
+            wrapperClass="!w-full !h-full cursor-move"
+            contentClass="!w-full !h-full"
+          >
+            <img
+              alt="Output"
+              className="mx-auto my-4 p-8"
+              height="800"
+              src={`http://localhost:8001/terravision/output?timestamp=${Date.now()}`}
+              style={{
+                height: '100%',
+                objectFit: 'contain'
+              }}
+              width="800"
+            />
+          </TransformComponent>
+        </TransformWrapper>
       )}
     </div>
   );
